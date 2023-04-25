f27845e2b348b4cd40eaec1274b55c9a962586b9
Dylan Addis-Thielen <addis2123@gmail.com>
2023-04-25 10:23:20 -0400

    Comment Updates.

--- GASS.py
+++ GASS.py
@@ -1,6 +1,6 @@
 ## Google Admin & Snipe-IT ##
 ## By: Dylan Addis-Thielen ##
-##       03/17/2023        ##
+##       04/25/2023        ##
 
 import csv # To do a CSV Import to Google Admin.
 import requests #To send and recieve requests from SnipeIT
@@ -54,14 +54,16 @@ url = keyfile.URL
 api_key = keyfile.API_KEY
 Asset_State = keyfile.STATESN
 
-def Get_UserByID(ID): #Find a User by there SnipeIT ID#
+#Find a User by there SnipeIT ID#
+def Get_UserByID(ID): 
 	ID = str(ID)
 	link = url+'/api/v1/users/'+ID
 	key = {"Authorization": "Bearer "+api_key}
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data 
 
-def Get_UserBySearch(TERM): #Find Users by information in SnipeIT
+#Find Users by information in SnipeIT
+def Get_UserBySearch(TERM): 
 	TERM = TERM.replace('@','%40')
 	TERM = TERM.replace(' ','%20')
 	link = url+'/api/v1/users?search='+TERM
@@ -69,47 +71,54 @@ def Get_UserBySearch(TERM): #Find Users by information in SnipeIT
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data
 
-def Get_All_Loc(): #Return All Locations in SnipeIT
+#Return All Locations in SnipeIT
+def Get_All_Loc(): 
 	link = url+'/api/v1/locations'
 	key = {"Authorization": "Bearer "+api_key}
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data 
 
-def Get_Loc(NAME): #Return Location with Name
+#Return Location with Name
+def Get_Loc(NAME): 
 	NAME = str(NAME)
 	link = url+'/api/v1/locations?name='+NAME
 	key = {"Authorization": "Bearer "+api_key}
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data 
 
-def Make_Loc(NAME): #Make Location NAME
+#Make Location NAME
+def Make_Loc(NAME): 
 	##Remember Payload == {}
 	link = url+'/api/v1/locations'
 	key = {"Accept": "application/json", "Authorization": "Bearer "+api_key,"Content-Type": "application/json"}
 	data =  json.loads(requests.request("POST", link, json={'name': NAME}, headers=key).text)
 	return data
 
-def Get_DevByID(ID): #Find a Device by its SnipeIT ID#
+#Find a Device by its SnipeIT ID#
+def Get_DevByID(ID): 
 	ID = str(ID)
 	link = url+'/api/v1/hardware/'+ID
 	key = {"Authorization": "Bearer "+api_key}
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data
 
-def Get_DevBySN(SN): #Find a Device by its SN in SnipeIT
+#Find a Device by its SN in SnipeIT
+def Get_DevBySN(SN): 
 	link = url+'/api/v1/hardware/byserial/'+SN
 	key = {"Authorization": "Bearer "+api_key}
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data
 
-def Get_DevByTag(TAG): #Find a Device by its Asset Tag in SnipeIT
+#Find a Device by its Asset Tag in SnipeIT
+def Get_DevByTag(TAG): 
 	TAG = str(TAG)
 	link = url+'/api/v1/hardware/bytag/'+TAG
 	key = {"Authorization": "Bearer "+api_key}
 	data =  json.loads(requests.request("Get", link, headers=key).text)
 	return data
 
-def Patch_DevByID(ID, payload): #Update a Device in SnipeIT by the ID#
+#Update a Device in SnipeIT by the ID#
+def Patch_DevByID(ID, payload): 
 	##Remember Payload == {}
 	ID=str(ID)
 	link = url+'/api/v1/hardware/'+ID
@@ -117,13 +126,15 @@ def Patch_DevByID(ID, payload): #Update a Device in SnipeIT by the ID#
 	data =  json.loads(requests.request("PATCH", link, json=payload, headers=key).text)
 	return data
 
-def Make_Dev(payload): #Make a new Device in SnipeIT
+#Make a new Device in SnipeIT
+def Make_Dev(payload): 
 	##Remember Payload == {}
 	link = url+'/api/v1/hardware'
 	key = {"Accept": "application/json", "Authorization": "Bearer "+api_key,"Content-Type": "application/json"}
 	data =  json.loads(requests.request("POST", link, json=payload, headers=key).text)
 	return data
 
+#check in a Device in SnipeIT by the ID#
 def Chkin_Dev(ID):
 	##Remember Payload == {}
 	ID=str(ID)
@@ -133,6 +144,7 @@ def Chkin_Dev(ID):
 	data =  json.loads(requests.request("POST", link, json=payload, headers=key).text)
 	return data
 
+#check out a Device in SnipeIT by the ID# and User ID#
 def Chkout_Dev(ID,USR):
 	##Remember Payload == {}
 	ID=str(ID) #Must be the DeviceID in SnipeIT
@@ -147,27 +159,32 @@ def Chkout_Dev(ID,USR):
 	data =  json.loads(requests.request("POST", link, json=payload, headers=key).text)
 	return data
 
-def Get_Ou(OrgPath): #Get OU info by OrgPath in Google Admin
+#Get OU info by OrgPath in Google Admin
+def Get_Ou(OrgPath): 
 	request=service.orgunits().get(customerId = customerId, orgUnitPath=OrgPath)
 	response=request.execute()
 	return response
 
-def Get_All_Ou(): #Get all OUs in Goolge Admin
+#Get all OUs in Goolge Admin
+def Get_All_Ou(): 
 	request=service.orgunits().list(customerId = customerId, type='all')
 	response=request.execute()
 	return response
 
-def Set_Ou(Org, ID): #Set the OU of a Device by OrgPath and Device ID
+#Set the OU of a Device by OrgPath and Device ID
+def Set_Ou(Org, ID): 
 	request=service.chromeosdevices().moveDevicesToOu(customerId = customerId, orgUnitPath=Org, body={"deviceIds":ID})
 	response=request.execute()
 	return response
 
-def Get_Cross(ID): #Get Device in Google Admin by DeviceID
+#Get Device in Google Admin by DeviceID
+def Get_Cross(ID): 
 	request=service.chromeosdevices().get(customerId = customerId, deviceId=ID, projection='FULL')
 	response=request.execute()
 	return response
 
-def Patch_Cross(ANOLOC, ANOUSR, NOTES, ASSET, ID): #Update a Device in Google Admin
+#Update a Device in Google Admin
+def Patch_Cross(ANOLOC, ANOUSR, NOTES, ASSET, ID): 
 	UPDT={
 		"annotatedLocation":ANOLOC,
 		 "annotatedUser":ANOUSR,
@@ -178,16 +195,23 @@ def Patch_Cross(ANOLOC, ANOUSR, NOTES, ASSET, ID): #Update a Device in Google Ad
 	response=request.execute()
 	return response
 
-def Update_G_Csv(CSV): # Using CSV, Update chromebooks in Google Admin CSV must have data in order (Anotated Location, Anotated User, Notes, Annotated Asset ID, Device ID)
-	with open(CSV, mode ='r')as file: # Get the CSV File
-		csvFile = csv.reader(file) # Read the CSV File
-		for lines in tqdm(csvFile): # Read the data one line at a time
+#Using CSV, Update chromebooks in Google Admin CSV must have data in order (Anotated Location, Anotated User, Notes, Annotated Asset ID, Device ID)
+def Update_G_Csv(CSV): 
+	#Get the CSV File
+	with open(CSV, mode ='r')as file: 
+		#Read the CSV File
+		csvFile = csv.reader(file) 
+		#Read the data one line at a time
+		for line in tqdm(csvFile): 
 			try:
-				Patch_Cross(lines[0], lines[1], lines[2], lines[3], lines[4]) #Update all fields based on the CSV. 
+				#Update all fields based on the CSV. 
+				Patch_Cross(line[0], line[1], line[2], line[3], line[4]) 
 			except:
-				print(lines) # Print the lines that failed.
+				#Print the lines that failed.
+				print(line) 
 
-def Get_Cross_ID(SL): #Get the Device ID from Google Admin by SN
+#Get the Device ID from Google Admin by SN
+def Get_Cross_ID(SL): 
 	CL=Get_All_Cross()
 	DIC={}
 	CrossID=[]
@@ -197,7 +221,8 @@ def Get_Cross_ID(SL): #Get the Device ID from Google Admin by SN
 		CrossID.append(DIC[SN])
 	return CrossID
 
-def Get_All_Cross(): #Get all Google Admin Devices
+#Get all Google Admin Devices
+def Get_All_Cross(): 
 	print("Gathering Chromebooks...")
 	request=service.chromeosdevices().list(customerId = customerId, orderBy='serialNumber', projection='FULL')
 	response=request.execute()
@@ -210,14 +235,15 @@ def Get_All_Cross(): #Get all Google Admin Devices
 		while not end:
 			cross=response.get('chromeosdevices', [])
 			listcros=listcros+cross
-			#print("\r"+str(len(listcros)), end="\r")
+			print("\r"+str(len(listcros)), end="\r")
 			nextpage=service.chromeosdevices().list_next(previous_request=request, previous_response=response)
 			if nextpage==None:
 				break
 			response=nextpage.execute()
 		return listcros
 
-def Print_Cross(crosslist): #Print all the information for devices in crosslist
+#Print all the information for devices in crosslist
+def Print_Cross(crosslist): 
 	for cross in crosslist:
 		try:
 			print(
